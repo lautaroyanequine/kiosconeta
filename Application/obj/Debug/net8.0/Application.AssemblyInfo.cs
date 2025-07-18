4cfaa996db0762465fdec6b4f061cf39c3349671
lauty <lautaroyanequine@gmail.com>
2025-07-18 11:18:25 -0300

    segundo commit

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("KIOSCONETA")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f12bc707f2414efce53ce54ce89b32b32cad9f2")]
-[assembly: System.Reflection.AssemblyProductAttribute("KIOSCONETA")]
-[assembly: System.Reflection.AssemblyTitleAttribute("KIOSCONETA")]
+[assembly: System.Reflection.AssemblyProductAttribute("Application")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
